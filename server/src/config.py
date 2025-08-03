2084f65e282abb82dab3ce6c6ebb8af5d4a63afc
Johan Book <{ID}+{username}@users.noreply.github.com>
2025-08-03 15:36:38 +0200

    refactor(server): use pydantic to parse settings

--- server/src/config.py
+++ server/src/config.py
@@ -1,12 +1,36 @@
 import json
 import logging
 import os
+import sys
+from typing import List
 
-CONFIG_PATH = os.environ.get("CONFIG_PATH", "./settings.json")
-
-with open(CONFIG_PATH, "r", encoding="utf-8") as file:
-    Config = json.load(file)
-
+from pydantic import BaseModel, ValidationError
 
 logging.basicConfig(format="%(levelname)s:\t%(message)s", level=logging.DEBUG)
-logging.debug(f"Configurations loaded from file '{CONFIG_PATH}'")
+
+
+class Location(BaseModel):
+    name: str
+    unique_roles: List[str]
+    nonunique_roles: List[str]
+
+
+class Settings(BaseModel):
+    locations: List[Location]
+
+
+SETTINGS_PATH = os.environ.get("SETTINGS_PATH", "./settings.json")
+
+try:
+    with open(SETTINGS_PATH, "r", encoding="utf-8") as file:
+        config_data = json.load(file)
+        SETTINGS = Settings.parse_obj(config_data)
+except FileNotFoundError:
+    logging.critical(f"Settings file not found at '{SETTINGS_PATH}'")
+    sys.exit()
+except ValidationError as error:
+    logging.critical(f"Invalid settings in file '{SETTINGS_PATH}':\n{error}")
+    sys.exit()
+
+
+logging.debug(f"Settings loaded from file '{SETTINGS_PATH}'")
